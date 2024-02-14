bae0236cd0eb96f7226fa96dc2f97fe5339f2c61
OsvaldoGS1 <osvaldo@posibilidades.com.mx>
2024-02-14 14:40:03 -0600

    Modelo de oficio, modificacion en el modelo usuario

---
+++ api_1/src/models/oficio.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+// const usuarioScheme = require("../models/usuario.model.js");
+
+const oficioScheme = mongoose.Schema({
+  usuario: {
+    type: Schema.Types.ObjectId,
+    ref: "usuarios",
+    required: true,
+  },
+  profesion: { type: String, required: true, index: true },
+  descripcion: { type: String, required: true },
+  metodos_pago: {
+    tarjeta: { type: Boolean, default: false },
+    transferencia: { type: Boolean, default: false },
+    efectivo: { type: Boolean, default: false },
+  },
+  celular: { type: String, required: true },
+  ubicacion: { type: String, required: true },
+  imagen: { type: String, required: true },
+  visita_cobro: { type: String, required: true },
+  direccion: {
+    calle: { type: String },
+    cp: { type: Number },
+    colonia: { type: String },
+    municipio: { type: String },
+    estado: { type: String },
+    pais: { type: String },
+    visible: { type: Boolean },
+  },
+  imagenes_trabajo: { type: [{ type: String }], required: false },
+  identificacion: { type: String, required: true },
+  selfie: { type: String, required: true },
+  carta_penal: { type: String, required: false },
+  referencia: { type: [{ type: String }], required: true },
+  estatus: {
+    type: String,
+    enum: ["Activo", "Inactivo", "Rechazado"],
+    index: true,
+  },
+  verificado: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// const
+
+module.exports = mongoose.model("oficios", oficioScheme);
